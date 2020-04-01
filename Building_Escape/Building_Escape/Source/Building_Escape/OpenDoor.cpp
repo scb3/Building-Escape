a7d6a7e424632803840d32d4d513b44494ae1b33
scb3 <scott.c.brooks@outlook.com>
2020-04-01 19:37:30 +0100

    Fixed Door

--- Building_Escape/Building_Escape/Source/Building_Escape/OpenDoor.cpp
+++ Building_Escape/Building_Escape/Source/Building_Escape/OpenDoor.cpp
@@ -3,6 +3,9 @@
 #include "OpenDoor.h"
 #include "Components/ActorComponent.h"
 #include "GameFramework/actor.h"
+#include "Components/ActorComponent.h"
+#include "Engine/World.h""
+#include "GameFramework/PlayerController.h"
 
 
 // Sets default values for this component's properties
@@ -11,23 +14,27 @@ UOpenDoor::UOpenDoor()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-
-	
+		
 }
 
 
 // Called when the game starts
 void UOpenDoor::BeginPlay()
 {
-	Super::BeginPlay();
+		Super::BeginPlay();
+
+		ActorThatOpens = GetWorld()->GetFirstPlayerController()->GetPawn();
+}
+
+void UOpenDoor::OpenDoor()
+{
+	
 
 	AActor* Owner = GetOwner();
 
-	FRotator NewRotation = FRotator(0.0f, 90.0f, 0.0f);
+	FRotator NewRotation = FRotator(0.0f, -90.0f, 0.0f);
 
 	Owner->SetActorRotation(NewRotation);
-	
 }
 
 
@@ -35,7 +42,10 @@ void UOpenDoor::BeginPlay()
 void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (PressurePlate && PressurePlate -> IsOverlappingActor(ActorThatOpens)) 
+	{
+		OpenDoor();
+	}
 
-	// ...
 }
 
